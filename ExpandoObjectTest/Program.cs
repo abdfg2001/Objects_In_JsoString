90b1cda949356b0a5feb4958543e6dea5695ea2e
abdfg2001 <abdfg2001@yahoo.com>
2025-05-20 00:23:45 -0400

    CompararUno.Comparar1 function fixed

--- ExpandoObjectTest/Program.cs
+++ ExpandoObjectTest/Program.cs
@@ -6,36 +6,43 @@ using System.Reflection;
 
 
 string json = @"[{""Codigo"":53},{ ""Name"": ""John"", ""Age"": 30 },{""Name"": ""Adrian"", ""Gen"": ""Male"", ""Height"": 12},{ ""Name"": ""Andres"", ""Age"": 14 },{ ""Sex"": ""Man"", ""Age"": 12 },{""Codigo"":19},{""Name"": ""Santa Claus"", ""Age"": 120, ""IsEmployed"":true,""Skills"":[""Gift"",""Toys"",""Merry Christmas""]}]";
-var JsonString = "[{\"Name\":\"John Doe\",\"Age\":30,\"IsEmployed\":true,\"Skills\":[\"C#\",\"JavaScript\",\"Python\"]},{\"Name\": \"Adrian\", \"Gen\": \"Male\", \"Height\": 12}]";
+var JsonString = "[{\"Name\": \"Rose\", \"Gen\": \"Female\", \"Height\": 17},{\"Name\":\"John Doe\",\"Age\":30,\"IsEmployed\":true,\"Skills\":[\"C#\",\"JavaScript\",\"Python\"]},{\"Name\": \"Adrian\", \"Gen\": \"Male\", \"Height\": 12},{\"Name\": \"Peter\", \"Gen\": \"Male\", \"Height\": 8}]";
 var JsonAlong = "{\"Name\":\"Santa Claus\",\"Age\":120,\"IsEmployed\":true,\"Skills\":[\"Gift\",\"Toys\",\"Merry Christmas\"]}";
+var JsonShort = "{\"Codigo\":19}";
 
 
-
-IPeople Personas = CompararUno.Comparar1<Person>(JsonAlong);
+IPeople Personas = CompararUno.Comparar1<PersonCodigo>(JsonShort);
 
 Console.WriteLine("*************compare between an object and an object in a json string list*************");
 
-var properties1 = Personas.GetType().GetProperties(BindingFlags.Public | BindingFlags.Instance);
-foreach (var property in properties1)
+if (Personas != null)
 {
-    if (property.CanWrite)
+    var properties = Personas.GetType().GetProperties(BindingFlags.Public | BindingFlags.Instance);
+    foreach (var property in properties)
     {
-        if (property.PropertyType == typeof(List<string>))
+        if (property.CanWrite)
         {
-            var list = (List<string>)property.GetValue(Personas)!;
-            foreach (var item1 in list)
+            if (property.PropertyType == typeof(List<string>))
             {
-                Console.WriteLine("Property: " + property.Name + " Value: " + item1);
+                var list = (List<string>)property.GetValue(Personas)!;
+                foreach (var item1 in list)
+                {
+                    Console.WriteLine("Property: " + property.Name + " Value: " + item1);
+                }
             }
+            else
+                Console.WriteLine("Property: " + property.Name + " Value: " + property.GetValue(Personas));
         }
-        else
-            Console.WriteLine("Property: " + property.Name + " Value: " + property.GetValue(Personas));
     }
+}
+else
+{
+    Console.WriteLine("No object found");
 }
 
 Console.WriteLine("************compare if there is an object in a list of json objects*************");
 
-List<IPeople> people = CompararUno.Comparar<PersonGen>(JsonString);
+List<IPeople> people = CompararUno.Comparar<Person>(JsonString);
 
 Console.WriteLine("***********************************");
 
